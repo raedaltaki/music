4b99d8d64aed7f98a868e92540178fb1ed993040
Raed Altaki <raed.simon@gmail.com>
2022-06-24 01:19:58 -0400

    added validation to login form

--- src/components/AuthModal.vue
+++ src/components/AuthModal.vue
@@ -50,106 +50,9 @@
             </ul>
 
             <!-- Login Form -->
-            <form v-show="tab === 'login'">
-                <!-- Email -->
-                <div class="mb-3">
-                <label class="inline-block mb-2">Email</label>
-                <input type="email"
-                    class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                    duration-500 focus:outline-none focus:border-black rounded"
-                    placeholder="Enter Email" />
-                </div>
-                <!-- Password -->
-                <div class="mb-3">
-                <label class="inline-block mb-2">Password</label>
-                <input type="password"
-                    class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                    duration-500 focus:outline-none focus:border-black rounded"
-                    placeholder="Password" />
-                </div>
-                <button type="submit"
-                class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
-                    hover:bg-purple-700">
-                Submit
-                </button>
-            </form>
+            <app-login-from v-if="tab === 'login'"/>
             <!-- Registration Form -->
-            <vee-form v-show="tab === 'register'" :validation-schema="schema"
-            @submit="register" :initial-values="userData">
-                <!-- Name -->
-                <div class="mb-3">
-                <label class="inline-block mb-2">Name</label>
-                <vee-field type="text" name="name"
-                    class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                    duration-500 focus:outline-none focus:border-black rounded"
-                    placeholder="Enter Name" />
-                <ErrorMessage class="text-red-600" name="name" />
-                </div>
-                <!-- Email -->
-                <div class="mb-3">
-                <label class="inline-block mb-2">Email</label>
-                <vee-field type="email" name="email"
-                    class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                    duration-500 focus:outline-none focus:border-black rounded"
-                    placeholder="Enter Email" />
-                <ErrorMessage class="text-red-600" name="email" />
-                </div>
-                <!-- Age -->
-                <div class="mb-3">
-                <label class="inline-block mb-2">Age</label>
-                <vee-field type="number" name="age"
-                    class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                    duration-500 focus:outline-none focus:border-black rounded" />
-                <ErrorMessage class="text-red-600" name="age" />
-                </div>
-                <!-- Password -->
-                <div class="mb-3">
-                    <label class="inline-block mb-2">Password</label>
-                    <vee-field name="password" :bails="false" v-slot="{ field, errors }">
-                        <input class="block w-full py-1.5 px-3 text-gray-800 border
-                            border-gray-300 transition duration-500 focus:outline-none
-                            focus:border-black rounded"
-                            type="password" placeholder="Password" v-bind="field" />
-                        <div class="text-red-600" v-for="error in errors" :key="error">
-                            {{ error }}
-                        </div>
-                    </vee-field>
-                </div>
-                <!-- Confirm Password -->
-                <div class="mb-3">
-                <label class="inline-block mb-2">Confirm Password</label>
-                <vee-field type="password" name="confirm_password"
-                    class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                    duration-500 focus:outline-none focus:border-black rounded"
-                    placeholder="Confirm Password" />
-                <ErrorMessage class="text-red-600" name="confirm_password" />
-                </div>
-                <!-- Country -->
-                <div class="mb-3">
-                <label class="inline-block mb-2">Country</label>
-                <vee-field as="select" name="country"
-                    class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                    duration-500 focus:outline-none focus:border-black rounded">
-                    <option value="USA">USA</option>
-                    <option value="Mexico">Mexico</option>
-                    <option value="Germany">Germany</option>
-                    <option value="Antarctica">Antarctica</option>
-                </vee-field>
-                <ErrorMessage class="text-red-600" name="country" />
-                </div>
-                <!-- TOS -->
-                <div class="mb-3 pl-6">
-                <vee-field type="checkbox" name="tos" value="1"
-                    class="w-4 h-4 float-left -ml-6 mt-1 rounded inline-block" />
-                <label class="inline-block">Accept terms of service</label>
-                <div><ErrorMessage class="text-red-600" name="tos" /></div>
-                </div>
-                <button type="submit"
-                class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
-                    hover:bg-purple-700">
-                Submit
-                </button>
-            </vee-form>
+            <app-register-from v-else/>
             </div>
         </div>
         </div>
@@ -158,24 +61,17 @@
 
 <script>
 import { mapMutations, mapState } from 'vuex';
+import AppLoginFrom from './LoginForm.vue';
+import AppRegisterFrom from './RegisterForm.vue';
 
 export default {
   name: 'AuthModal',
+  components: {
+    AppLoginFrom, AppRegisterFrom,
+  },
   data() {
     return {
       tab: 'login',
-      schema: {
-        name: 'required|min:3|max:100|alpha_spaces',
-        email: 'required|min:3|max:100|email',
-        age: 'required|min_value:18|max_value:100',
-        password: 'required|min:3|max:100|alpha_spaces',
-        confirm_password: 'passwords_mismatch:@password',
-        country: 'required|counrty_excluded:Antarctica',
-        tos: 'tos',
-      },
-      userData: {
-        country: 'USA',
-      },
     };
   },
   computed: {
@@ -191,7 +87,7 @@ export default {
   },
   methods: {
     ...mapMutations(['toggleAuthModal']),
-    register(values) {
+    login(values) {
       console.log(values);
     },
   },
