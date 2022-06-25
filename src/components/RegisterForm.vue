3b5b0b8eac4584dd9df6f36c873f3c96877b8682
Raed Altaki <raed.simon@gmail.com>
2022-06-25 13:37:54 -0400

    added connection to firebase and added the functionality to register form and store users in DB

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -82,6 +82,8 @@
 </template>
 
 <script>
+import { auth, usersCollection } from '@/includes/firebase';
+
 export default {
   name: 'RegisterForm',
   data() {
@@ -105,12 +107,38 @@ export default {
     };
   },
   methods: {
-    register(values) {
+    async register(values) {
       this.reg_show_alert = true;
       this.reg_in_submission = true;
       this.reg_alert_variant = 'bg-blue-500';
       this.reg_alert_msg = 'Please wait! Your account is being created.';
 
+      let userCred = null;
+      try {
+        userCred = await auth
+          .createUserWithEmailAndPassword(values.email, values.password);
+      } catch (error) {
+        this.reg_in_submission = false;
+        this.reg_alert_variant = 'bg-red-500';
+        this.reg_alert_msg = 'An unexpected error accured. Please try again later.';
+        return;
+      }
+
+      try {
+        await usersCollection.add({
+          name: values.name,
+          email: values.email,
+          age: values.age,
+          country: values.country,
+        });
+      } catch (error) {
+        this.reg_in_submission = false;
+        this.reg_alert_variant = 'bg-red-500';
+        this.reg_alert_msg = 'An unexpected error accured. Please try again later.';
+        return;
+      }
+
+      console.log(userCred);
       this.reg_alert_variant = 'bg-green-500';
       this.reg_alert_msg = 'Success! Your account has been created';
       console.log(values);
---
+++ src/includes/firebase.js
@@ -0,0 +1,30 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCB8gcrObmZUgLvAaxw1YvZ5dsWvsvMA2Y',
+  authDomain: 'music-6afe7.firebaseapp.com',
+  projectId: 'music-6afe7',
+  storageBucket: 'music-6afe7.appspot.com',
+  messagingSenderId: '354319152807',
+  appId: '1:354319152807:web:03060d2b07290cca1f0d82',
+  measurementId: 'G-KE3HK1P1NL',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.getAnalytics(app);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+const usersCollection = db.collection('users');
+
+export {
+  auth,
+  db,
+  usersCollection,
+};
