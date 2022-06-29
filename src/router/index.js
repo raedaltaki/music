aac7d604e26ba1242dae995a8230670b730c9b78
Raed Altaki <raed.simon@gmail.com>
2022-06-29 00:52:24 -0400

    added routing and manage page

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import HomeView from '@/views/HomeView.vue';
+import AboutView from '@/views/AboutView.vue';
+import ManageView from '@/views/ManageView.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    // name: 'home',
     component: HomeView,
   },
   {
     path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    component: AboutView,
+  },
+  {
+    path: '/manage',
+    component: ManageView,
   },
 ];
 
